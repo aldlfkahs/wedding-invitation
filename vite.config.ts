2f11c9ffc709c029ab162d401e864da04d6c1aae
aldlfkahs <aldlfkahs@gmail.com>
2025-12-31 23:44:59 +0900

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/wedding-invitation/', // GitHub Pages용 base 경로 추가
   plugins: [react()],
   server: {
     port: 3000,
